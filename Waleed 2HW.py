0208fbc76c65bc078cbaebe56df0a19b5beeefef
Alwaleed1600 <114076116+Alwaleed1600@users.noreply.github.com>
2022-09-25 13:15:28 +0300

    Add files via upload

---
+++ Waleed 2HW.py
@@ -0,0 +1,37 @@
+
+1.	Output: 4
+•	Code: lan
+
+2.	Output: 2
+•	Code: print(list[0])
+
+3.	Output: 2
+•	Code: print(list.count(0))
+
+4.	Output: IndexError: list index out of 
+•	Code: print(list[4])
+
+5.	Output: True
+•	Code: 2 in list
+
+6.	Output: [2, 0, 1, 0, 'A']
+•	Code: list.append("A")print(list)
+
+7.	Output: [0, 0, 1, 2]
+•	Code:  list.sort()
+	print(list)
+
+8.	Output: [2, 0, 1]
+•	Code: 
+	list.pop(1)
+	print(list)
+
+9.	Output: [0, 1]
+•	Code: 
+list: list =[2,0,1,0]
+print(list[1:3])
+
+10.	Output: [0, 1, 0, 2]
+•	Code: 
+list.reverse()
+ print(list)
